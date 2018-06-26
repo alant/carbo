3f2cd149dacc3ab64faf276cb317af205b78414a
Yuan Jiang <jiangyuan916@gmail.com>
2018-06-26 15:02:49 -0700

    add model for Cow

---
+++ src/utils/GameConst.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by SPC
+ * game const
+ */
+class GameConst{
+    public static SCENT_WIDTH:number = 0;
+    public static SCENT_HEIGHT:number = 0;
+    public static GamePoxX:number = 0;
+}
