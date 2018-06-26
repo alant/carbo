9fcb7d47dfc30ea3bc70bb430ed6a3f50684bfbb
Tal Rasha <talrasha007@gmail.com>
2018-06-26 16:05:23 -0700

    UserInfo

---
+++ contracts/contracts/UserInfo.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.23;
+
+contract UserInfo {
+    mapping(bytes32 => address) public addressOf;
+    mapping(address => bytes32) public nameOf;
+    mapping(address => string) public avatarOf;
+
+    function canRegister(bytes32 name) public view returns (bool) {
+        return addressOf[name] == address(0);
+    }
+
+    function register(bytes32 name, string avatarUrl) public {
+        require(canRegister(name));
+
+        addressOf[name] = msg.sender;
+        nameOf[msg.sender] = name;
+        avatarOf[msg.sender] = avatarUrl;
+    }
+
+    function setAvatar(string avatarUrl) public {
+        avatarOf[msg.sender] = avatarUrl;
+    }
+}
