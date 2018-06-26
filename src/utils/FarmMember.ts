6f07daa32a47abd7ef1d5bac0ed4828b9a79f0e9
Yuan Jiang <jiangyuan916@gmail.com>
2018-06-26 15:03:37 -0700

    add model for farm membver

---
+++ src/utils/FarmMember.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by SPC
+ * data struture of Farm member
+ */
+class FarmMember {
+	public name: string;
+	public walletAddress: string;
+    public balance: number;
+    public cows: Array<Cow>;
+}
