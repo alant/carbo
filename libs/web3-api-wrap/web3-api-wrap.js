bd303b10172b56e6b634d288f71d3ff757c19714
Yuan Jiang <jiangyuan916@gmail.com>
2018-06-27 08:39:10 -0700

    backend

--- libs/web3-api-wrap/web3-api-wrap.js
+++ libs/web3-api-wrap/web3-api-wrap.js
@@ -1,52 +1,69 @@
-const Web3 = require('web3');
+// const Web3 = require('web3');
 
-function promisify(fn) {
-  return function () {
-    const me = this;
-    const args = Array.prototype.slice.call(arguments, 0);
+(function () {
+  function promisify(fn) {
+    return function () {
+      const me = this;
+      const args = Array.prototype.slice.call(arguments, 0);
 
-    if (typeof args[args.length - 1] === 'function') {
-      return fn.apply(me, args);
-    } else {
-      return new Promise(function (resolve, reject) {
-        args.push(function (err, res) {
-          if (err) reject(err);
-          else resolve(res);
+      if (typeof args[args.length - 1] === 'function') {
+        return fn.apply(me, args);
+      } else {
+        return new Promise(function (resolve, reject) {
+          args.push(function (err, res) {
+            if (err) reject(err);
+            else resolve(res);
+          });
+
+          fn.apply(me, args);
         });
-
-        fn.apply(me, args);
-      });
+      }
     }
   }
-}
 
-let web3;
+  let web3;
 
-if (window.web3) {
-  web3 = new Web3(window.web3.currentProvider);
+  if (window.web3) {
+    web3 = new Web3(window.web3.currentProvider);
 
-  window.web3.eth.getAccounts((err, accounts) => {
-    if (!err) web3.eth.defaultAccount = accounts[0];
-  });
-
-  web3.eth.getAccounts = promisify(web3.eth.getAccounts);
-  web3.eth.getBalance = promisify(web3.eth.getBalance);
-  web3.eth.getBlockNumber = promisify(web3.eth.getBlockNumber);
-  web3.eth.getBlock = promisify(web3.eth.getBlock);
-  web3.eth.sendTransaction = promisify(web3.eth.sendTransaction);
-  web3.eth.getTransactionReceipt = promisify(web3.eth.getTransactionReceipt);
-
-  web3.loadContract = web3.eth.loadContract = function (abi, address) {
-    const instance = web3.eth.contract(abi).at(address);
-
-    abi.forEach(api => {
-      if (api.type === 'function') {
-        instance[api.name] = promisify(instance[api.name]);
-      }
+    window.web3.eth.getAccounts((err, accounts) => {
+      if (!err) web3.eth.defaultAccount = accounts[0];
     });
 
-    return instance;
-  }
-}
+    web3.eth.getAccounts = promisify(web3.eth.getAccounts);
+    web3.eth.getBalance = promisify(web3.eth.getBalance);
+    web3.eth.getBlockNumber = promisify(web3.eth.getBlockNumber);
+    web3.eth.getBlock = promisify(web3.eth.getBlock);
+    web3.eth.sendTransaction = promisify(web3.eth.sendTransaction);
+    web3.eth.getTransactionReceipt = promisify(web3.eth.getTransactionReceipt);
 
-// module.exports = { web3 };
+    web3.loadContract = web3.eth.loadContract = function (abi, address) {
+      const instance = web3.eth.contract(abi).at(address);
+
+      abi.forEach(api => {
+        if (api.type === 'function') {
+          instance[api.name] = promisify(instance[api.name]);
+        }
+      });
+
+      return instance;
+    }
+  }
+
+  window.web3wrap = web3;
+  window.loadJsonRes = path => new Promise(resolve => {
+    RES.getResByUrl(path, resolve, null, 'json');
+  });
+
+  let addresses;
+
+  window.getContractInstance = async (name, path) => {
+    addresses = addresses || await loadJsonRes('contracts/build/contract_addresses.json');
+    const className = name.substring(0, 1).toUpperCase() + name.substring(1);
+
+    path = path || `contracts/build/contracts/${className}.json`;
+    const data = await loadJsonRes(path);
+
+    return web3wrap.loadContract(data.abi, addresses[name]);
+  };
+})();
